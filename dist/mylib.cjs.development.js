a3ab09467494be0d1a6a31498ae49dc5c265fd42
Thammarith <8011543+thammarith@users.noreply.github.com>
2022-08-30 16:38:06 +0700

    Install tsdx

---
+++ dist/mylib.cjs.development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+var Thing = function Thing() {
+  return React.createElement("div", null, "the snozzberries taste like snozzberries");
+};
+
+exports.Thing = Thing;
+//# sourceMappingURL=mylib.cjs.development.js.map
